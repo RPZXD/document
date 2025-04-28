1ac066a7f53d1d39ac9ffb43be9085977a399a6d
RPZXD <rpz.tiwa@phichai.ac.th>
2025-04-28 10:25:11 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,28 +3,3 @@ session_start();
 session_unset();
 session_destroy();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Logout</title>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="utils.js"></script>
-</head>
-<body>
-<script>
-    Swal.fire({
-        icon: 'success',
-        title: 'ออกจากระบบเรียบร้อยแล้ว',
-        text: 'คุณได้ออกจากระบบสำเร็จ',
-        confirmButtonText: 'ตกลง',
-        timer: 1500,
-        willClose: () => {
-            window.location.href = 'login.php?logout=1';
-        }
-    }).then(() => {
-        window.location.href = 'login.php?logout=1';
-    });
-</script>
-</body>
-</html>
